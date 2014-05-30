2b067c7d66cf7e0ac22f6594ca6abeb0ce982159
tamarabernad <tamara@codedazur.es>
2014-05-30 12:45:08 +0200

    Initial Commit

---
+++ TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Tamara Bernad on 30/05/14.
+//  Copyright (c) 2014 Code d'Azur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDAAppDelegate class]));
+    }
+}
